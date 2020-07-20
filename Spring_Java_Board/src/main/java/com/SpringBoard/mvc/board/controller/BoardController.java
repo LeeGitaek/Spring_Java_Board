89ad8529201cfcb0999bad81d5df86a4eaebc77f
GitaekLee <coolfamily77@gmail.com>
2020-07-20 20:47:06 +0900

    @RequestMapping => /board/read/{seq} BoardController

--- Spring_Java_Board/src/main/java/com/SpringBoard/mvc/board/controller/BoardController.java
+++ Spring_Java_Board/src/main/java/com/SpringBoard/mvc/board/controller/BoardController.java
@@ -3,6 +3,7 @@ package com.SpringBoard.mvc.board.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -19,4 +20,12 @@ public class BoardController {
 		model.addAttribute("boardList",boardService.list());
 		return "/board/list";
 	}
+	
+	@RequestMapping(value="/board/read/{seq}")
+	public String read(Model model, @PathVariable int seq) {
+		model.addAttribute("boardVO",boardService.read(seq));
+		return "/board/read";
+	}
+	
+	
 }
