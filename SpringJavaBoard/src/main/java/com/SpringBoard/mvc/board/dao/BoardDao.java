0e8b863538c60807489ac84788fc490b0f3abbc6
GitaekLee <coolfamily77@gmail.com>
2020-07-20 16:27:02 +0900

    commit again

---
+++ SpringJavaBoard/src/main/java/com/SpringBoard/mvc/board/dao/BoardDao.java
@@ -0,0 +1,21 @@
+package com.SpringBoard.mvc.board.dao;
+
+import java.util.List;
+import com.SpringBoard.mvc.board.domain.BoardVO;
+
+public interface BoardDao {
+	public abstract List<BoardVO> list();
+	
+	public abstract int delete(BoardVO boardVO);
+	
+	public abstract int deleteAll();
+	
+	public abstract int update(BoardVO boardVO);
+	
+	public abstract void insert(BoardVO boardVO);
+	
+	public abstract BoardVO select(int seq);
+	
+	public abstract int updateReadCount(int seq);
+	 
+}
---
+++ SpringJavaBoard/src/main/java/com/SpringBoard/mvc/board/service/BoardService.java
@@ -0,0 +1,16 @@
+package com.SpringBoard.mvc.board.service;
+
+import java.util.List;
+import com.SpringBoard.mvc.board.domain.BoardVO;
+
+public interface BoardService {
+	public abstract List<BoardVO> list();
+	
+	public abstract int delete(BoardVO boardVO);
+	
+	public abstract int edit(BoardVO boardVO);
+	
+	public abstract void write(BoardVO boardVO);
+	
+	public abstract BoardVO read(int seq);
+}
