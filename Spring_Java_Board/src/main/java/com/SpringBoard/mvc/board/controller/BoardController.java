2878e85ee6f1d5c1952ea076cb9358b3ef44f77b
GitaekLee <coolfamily77@gmail.com>
2020-07-20 19:55:12 +0900

    boardcontroller - add model @requestmapping

--- Spring_Java_Board/src/main/java/com/SpringBoard/mvc/board/controller/BoardController.java
+++ Spring_Java_Board/src/main/java/com/SpringBoard/mvc/board/controller/BoardController.java
@@ -2,6 +2,7 @@ package com.SpringBoard.mvc.board.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -14,7 +15,8 @@ public class BoardController {
 	
 	@RequestMapping(value="/board/list")
 	@ResponseBody
-	public String list() {
-		return boardService.list().toString();
+	public String list(Model model) {
+		model.addAttribute("boardList",boardService.list());
+		return "/board/list";
 	}
 }
