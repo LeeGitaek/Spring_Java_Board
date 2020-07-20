73ffc0eec659d67507fc67ace598898fa7542e77
GitaekLee <coolfamily77@gmail.com>
2020-07-20 13:42:15 +0900

    - BoardVO => VO 클래스 생성 - MyBatis => MyBatis 의 sqlmapclient 에 psa 를 적용한 어댑터를 빈으로 등록 - root-context.xml => sqlsessionfactory & sqlsessiontemplate 을 스프링 빈으로 등록
    - src/main/resources/sqlmap/config => mybatis-config.xml 생성

---
+++ SpringBoard/src/main/java/com/SpringBoard/mvc/board/domain/BoardVO.java
@@ -0,0 +1,28 @@
+package com.SpringBoard.mvc.board.domain;
+
+import java.sql.Timestamp;
+
+@Alias("boardVO")
+public class BoardVO {
+	private int seq;
+	private String title;
+	private String content;
+	private String writer;
+	private int password;
+	private Timestamp regDate;
+	private int cnt = 0; // 초기화 
+	
+	
+	public BoardVO() {}
+	
+	//public BoardVO(String title,String content,String writer, int password) { }
+	//생성자에 매개변수가 4개 이상이기 때문에 세터 메서드를 적용 
+	public void setBoardTitle(String strval) { title = strval; }
+	public void setBoardContent(String strval) { content = strval; }
+	public void setBoardWriter(String strval) { writer = strval; }
+	public void setBoardPassword(int numval) { password = numval; }
+	public void setBoardCnt(int numval) { cnt = numval; }
+	
+}
+
+
