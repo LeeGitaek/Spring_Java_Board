24d413bebd6f095618603eacb88dc2551ebddcbe
GitaekLee <coolfamily77@gmail.com>
2020-07-21 23:35:16 +0900

    - 자동 바인딩 / 세션에 대한 작업 처리

--- Spring_Java_Board/src/main/java/com/SpringBoard/mvc/board/controller/BoardController.java
+++ Spring_Java_Board/src/main/java/com/SpringBoard/mvc/board/controller/BoardController.java
@@ -10,11 +10,14 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.support.SessionStatus;
 
 import com.SpringBoard.mvc.board.domain.BoardVO;
 import com.SpringBoard.mvc.board.service.BoardService;
 
 @Controller
+@SessionAttributes("boardVO")
 public class BoardController {
 	@Autowired
 	private BoardService boardService;
@@ -59,6 +62,31 @@ public class BoardController {
 		}
 	}
 	
+	@RequestMapping(value="/board/edit/{seq}", method=RequestMethod.GET)
+	public String edit(@PathVariable int seq, Model model) {
+		BoardVO boardVO = boardService.read(seq);
+		model.addAttribute("boardVO",boardVO);
+		return "/board/edit";
+	}
 	
+	@RequestMapping(value="/board/edit/{seq}",method=RequestMethod.GET)
+	public String edit(@Valid BoardVO boardVO, BindingResult result , int pwd,SessionStatus sessionStatus, Model model) {
+		if(result.hasErrors()) 
+		{
+			return "/board/edit";
+		}
+		else 
+		{
+			if(boardVO.getPassword() == pwd) 
+			{
+				boardService.edit(boardVO);
+				sessionStatus.setComplete();
+				return "redirect:/board/list";
+			}
+			
+			model.addAttribute("msg","비밀번호가 일치하지 않습니다.");
+			return "/board/edit";
+		}
+	}
 	
 }
--- Spring_Java_Board/src/main/java/com/SpringBoard/mvc/board/domain/BoardVO.java
+++ Spring_Java_Board/src/main/java/com/SpringBoard/mvc/board/domain/BoardVO.java
@@ -29,6 +29,62 @@ public class BoardVO {
 	public void setBoardPassword(int numval) { password = numval; }
 	public void setBoardCnt(int numval) { cnt = numval; }
 	
+	public int getSeq() {
+		return seq;
+	}
+
+	public void setSeq(int seq) {
+		this.seq = seq;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+	public String getWriter() {
+		return writer;
+	}
+
+	public void setWriter(String writer) {
+		this.writer = writer;
+	}
+
+	public int getPassword() {
+		return password;
+	}
+
+	public void setPassword(int password) {
+		this.password = password;
+	}
+
+	public Timestamp getRegDate() {
+		return regDate;
+	}
+
+	public void setRegDate(Timestamp regDate) {
+		this.regDate = regDate;
+	}
+
+	public int getCnt() {
+		return cnt;
+	}
+
+	public void setCnt(int cnt) {
+		this.cnt = cnt;
+	}
+	
 }
 
 
